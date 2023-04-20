ff52b44efa57f246bd50bfb0a6099ca022b8dfe2
Carmina Familar <Carmina@MacBook-Pro-4.local>
2023-04-20 22:52:59 +0930

    modified ex00/megaphone.cpp

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
-char *make_upper(char *str)
+std::string make_upper(std::string str)
 {
-    for (int i = 0; i < std::strlen(str); i++)
+    for (int i = 0; i < str.length(); i++)
     {
         str[i] = std::toupper(str[i]);
     }
@@ -15,8 +15,14 @@ int main(int argc, char **argv)
     {
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
     }
-    for (int i = 1; i < argc; i++)
+    else
     {
-        std::cout << make_upper(argv[i]);
+        for (int i = 1; i < argc; i++)
+        {
+            std::string s(argv[i]);
+            std::cout << make_upper(s);
+        }
     }
+     std::cout << std::endl;
+    return (0);
 }
